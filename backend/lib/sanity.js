e883e31bc2d0adcf7a7786b69c3f83e659210fd4
Scar2345 <145857731+Scar2345@users.noreply.github.com>
2025-06-25 16:31:35 +0530

    some more changes

---
+++ backend/lib/sanity.js
@@ -0,0 +1,9 @@
+
+import sanityClient from '@sanity/client'
+
+export const client = sanityClient({
+  projectId: 'lutkiuk4', 
+  dataset: 'production',
+  apiVersion: '2024-06-25',
+  useCdn: true,
+})
